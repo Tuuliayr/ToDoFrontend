caed52d24bfefce5720efb60e3c67be2cbbb4297
JenninaFarm <56800256+JenninaFarm@users.noreply.github.com>
2020-12-07 17:21:40 +0200

    Add withRouter to task- and listButtons.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import ListView from "./ListView.js";
 import CreateTaskView from "./CreateTaskView.js";
 import TopBar from "./components/TopBar.js";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
 //    JOTAIN MAHDOLLISIA VIEWEJÄ
 import TaskView from "./TaskView.js";
 //import CreateTaskView from "./CreateTaskView.js";
-//import CreateList???
 
 class App extends React.Component {
   //state jossa tieto mikä view kyseessä???
@@ -21,14 +19,10 @@ class App extends React.Component {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/tasks">Tasks</Link>
-          </li>
-          <li>
             <Link to="/create-task">Create task</Link>
           </li>
-
           <Route exact path="/" component={ListView} />
-          <Route exact path="/tasks" component={TaskView} />
+          <Route exact path={"/list:listId([0-9]+)"} component={TaskView} />
           <Route exact path="/create-task" component={CreateTaskView} />
         </div>
       </Router>
