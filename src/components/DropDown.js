b1f86052fbcb652dc381b78a2bf22b9d9e7d9ccc
Tuuli <tuuli.ayravainen@tuni.fi>
2020-12-07 17:30:45 +0200

    Edit DropDown.js.

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -6,21 +6,78 @@ import {
   DropdownItem,
 } from "reactstrap";
 
-const DropDown = (props) => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+class DropDown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropDownValue: "Select priority",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  const toggle = () => setDropdownOpen((prevState) => !prevState);
+  handleChange(event) {
+    this.setState({ dropDownValue: event.target.dropDownValue });
+  }
 
-  return (
-    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-      <DropdownToggle caret>Priority</DropdownToggle>
-      <DropdownMenu>
-        <DropdownItem>1</DropdownItem>
-        <DropdownItem>2</DropdownItem>
-        <DropdownItem>3</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-  );
-};
+  handleSubmit(event) {
+    alert("Your favorite flavor is: " + this.state.dropDownValue);
+    event.preventDefault();
+  }
+
+  // dropDown = (props) => {
+  //   const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  //toggle = () => setDropdownOpen((prevState) => !prevState);
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Choose priority:
+          <br />
+          <select value={this.state.dropDownValue} onChange={this.handleChange}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+    // render() {
+    //   return (
+    //     <Dropdown>
+    //       <DropdownToggle
+    //         dropDownValue={this.state.dropDownValue}
+    //         onChange={this.handleChange}
+    //       ></DropdownToggle>
+    //       <DropdownMenu>
+    //         <DropdownItem>1</DropdownItem>
+    //         <DropdownItem>2</DropdownItem>
+    //         <DropdownItem>3</DropdownItem>
+    //       </DropdownMenu>
+    //     </Dropdown>
+    //   );
+  }
+}
+
+// const DropDown = (props) => {
+//   const [dropdownOpen, setDropdownOpen] = useState(false);
+
+//   const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+//   return (
+//     <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+//       <p>Choose priority:</p>
+//       <DropdownToggle caret>Priority</DropdownToggle>
+//       <DropdownMenu>
+//         <DropdownItem>1</DropdownItem>
+//         <DropdownItem>2</DropdownItem>
+//         <DropdownItem>3</DropdownItem>
+//       </DropdownMenu>
+//     </Dropdown>
+//   );
+// };
 
 export default DropDown;
