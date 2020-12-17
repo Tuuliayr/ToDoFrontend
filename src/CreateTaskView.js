d660a14bd645af2177838149e0a03f076f0220d8
Tuuli <tuuli.ayravainen@tuni.fi>
2020-12-17 14:23:48 +0200

    Edit CreateTaskView.js.

--- src/CreateTaskView.js
+++ src/CreateTaskView.js
@@ -6,6 +6,73 @@ import Calendar from "./components/CalendarButton.js";
 import TopBar from "./components/TopBar.js";
 import axios from "axios";
 
+// const CreateTaskView = (props) => {
+//   const [priority, setPriority] = React.useState("");
+
+//   const handleChange = (event) => {
+//     setPriority(event.target.value);
+//   };
+
+//   const handleSubmit = (event) => {
+//     this.isMounted = true;
+//     axios({
+//       method: "post",
+//       url: "/task",
+//       baseURL: "https://tamk-4a00ez62-3001-group10.herokuapp.com/api",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//       },
+//       data: {
+//         name: "Tiskaapa",
+//         priority: priority,
+//         list_id: 2,
+//       },
+//     })
+//       .then((result) => {
+//         if (this.isMounted) {
+//           this.setState({
+//             news: result.data.hits,
+//           });
+//         }
+//         return () => {
+//           this.isMounted = false;
+//         };
+//       })
+//       .then((response) => console.log(response))
+//       .catch((err) => console.log(err));
+//     event.preventDefault();
+//   };
+
+//   return (
+//     <div>
+//       <TopBar className="topBar" name="Create Task" return={true} />
+//       <div className="createTaskForm">
+//         <div onSubmit={handleSubmit}>
+//           <p>Task name:</p>
+//           <input
+//             type="text"
+//             placeholder="Task name..."
+//             style={{ marginBottom: "2rem" }}
+//           />
+//           <p>Deadline:</p>
+//           <Calendar />
+//           <br />
+//           <p>Add to list:</p>
+//           <ListDropDown />
+//           <br />
+//           <p>Choose priority:</p>
+//           <PriDropDown value={priority} onChange={handleChange} />
+//           <br />
+//           <button type="submit">Create task</button>
+//           {/* <input type="text" value={this.state.priority} /> */}
+//           {/* <button onClick={this.handleChange.bind(this)}>Add task</button> */}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
 class CreateTaskView extends React.Component {
   constructor(props) {
     super(props);
@@ -28,7 +95,7 @@ class CreateTaskView extends React.Component {
       url: "/task",
       baseURL: "https://tamk-4a00ez62-3001-group10.herokuapp.com/api",
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": "application/json",
         Accept: "application/json",
       },
       data: {
@@ -65,7 +132,7 @@ class CreateTaskView extends React.Component {
             <PriDropDown
               // priority={this.state.priority}
               name="pri"
-              value={this.state.pri}
+              value={this.state.priority}
               onChange={this.handleChange.bind(this)}
             />
             <br />
